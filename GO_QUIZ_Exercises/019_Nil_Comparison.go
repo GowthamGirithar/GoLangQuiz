f054db9fc3831b0273076f841abd835d6be4b48d
Gowtham Girithar Srirangasamy <gowthamgirithar@gmail.com>
2019-09-06 11:44:52 +0530

    Update 019_Nil_Comparison.go

--- GO_QUIZ_Exercises/019_Nil_Comparison.go
+++ GO_QUIZ_Exercises/019_Nil_Comparison.go
@@ -1,6 +1,6 @@
 
 package main
-
+type functionTest func() (string) //only function syntax
 func main() {
 	var m map[string]int //A
 	m["a"] = 1
@@ -12,6 +12,11 @@ func main() {
 	var m1 map[string]interface{} //c
 	m1["a"] = 1
 	if v := m1["b"]; v != nil { //D//but if we have the interfacetype , then it can be checked with nil
+		println(v)
+	}
+	
+	var m2 map[string]functionTest //E
+	if v := m2["b"]; v != nil { //F/NO ERROR, func type can be compare with nil
 		println(v)
 	}
 }
